ae5efe46d51cf7f5ee019b6d3885e37fac007604
muhamed-aroui <muhamed.aroui@gmail.com>
2024-01-08 22:37:22 +0100

    updated

--- src/configuration.py
+++ src/configuration.py
@@ -1,10 +1,38 @@
-
-data_config = dict(
-    evaluate= False,
-    db_root = "path/train",
-    test_root = "path/test",
-    validation_root="path/val",
-    normalize = dict(l1 = False, l2= False)
+training_config = dict(
+    epoch = 10,
+    batch_size = 256,
+    num_workers = 4,
+    log_iter = 1
 )
 
-config = dict(data_config = data_config)
+optim_config = dict(
+    optim = "adam",
+    lr = 3e-4
+)
+
+validation_config = dict(
+    early_stopping_epoch    = 50,
+    test_accuracy_log_epoch = 1
+)
+
+model_config = dict(
+    layer_type     = "GCN",
+    embedding_size = 512,
+    heads          = 4,
+    dropout        = 0.6,
+)
+
+data_config = dict(
+    evaluate        = False,
+    num_classes     = None,
+    db_root         = "path/train",
+    test_root       = "path/test",
+    validation_root = "path/val",
+    normalize       = dict(l1 = False, l2= False)
+)
+
+config = dict(training_config   = training_config,
+              optim_config      = optim_config,
+              validation_config = validation_config,
+              model_config      = model_config,
+              data_config       = data_config)
---
+++ src/models.py
@@ -0,0 +1,37 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv, GATv2Conv
+from torch_geometric.nn import global_mean_pool
+
+
+class GCN(nn.Module):
+    def __init__(self, node_features, embedding_size, num_classes, dropout):
+        super(GCN, self).__init__()
+        self.conv1 = GCNConv(node_features, embedding_size)
+        self.conv2 = GCNConv(embedding_size, embedding_size)
+        self.conv3 = GCNConv(embedding_size, embedding_size)
+        self.lin = nn.Linear(embedding_size, num_classes)
+        self.dropout = dropout
+
+    def forward(self, x, edge_index, batch):
+        # 1. Obtain node embeddings 
+        x = self.conv1(x, edge_index)
+        x = x.relu()
+        x = self.conv2(x, edge_index)
+        x = x.relu()
+        x = self.conv3(x, edge_index)
+
+        # 2. Readout layer
+        x = global_mean_pool(x, batch)  # [batch_size, hidden_channels]
+
+        # 3. Apply a final classifier
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.lin(x)
+        
+        return x
+def get_model(config, logger):
+    if config["model_config"]["layer_type"] == "GCN":
+        return GCN(node_features  = config["data_config"]["node_features"], 
+                   embedding_size = config["model_config"]["embedding_size"],
+                   num_classes    = config["data_config"]["num_classes"],
+                   dropout        = config["model_config"]["dropout"])
